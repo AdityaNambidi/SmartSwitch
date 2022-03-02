25d752e0834736b8dee77c75cfd9722cf1c0fa09
Aditya Nambidi <aditya@pop-os.localdomain>
2022-03-02 16:52:53 +0400

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './components/styles/style.css'
+import NavBar from "./components/NavBar";
+
+
+function App() {
+
+    return (
+        <div className="App">
+            
+            <NavBar />
+            <h1>Hello World</h1>
+            
+        </div>
+    );
+
+}
+
+export default App;
